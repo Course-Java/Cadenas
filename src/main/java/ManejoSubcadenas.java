16d4b4d1c9473a38518d1315cbc3d6658f2566c6
m4rcous <m.gongora.sanchez08@gmail.com>
2025-01-03 13:11:53 -0500

    feat: learn about substrings manage

---
+++ src/main/java/ManejoSubcadenas.java
@@ -0,0 +1,23 @@
+public class ManejoSubcadenas {
+    public static void main(String[] args) {
+        // Tema de subcadenas
+
+        var cadena1 = "Hola Mundo";
+        System.out.println("cadena1 = " + cadena1);
+
+        // Subcadena
+        var subcadena1 = cadena1.substring(0, 4);
+        System.out.println("subcadena1 = " + subcadena1);
+
+        // Subcadena "Mundo"
+        //var subcadena2 = cadena1.substring(5);
+        var subcadena2 = cadena1.substring(5, 10);
+        System.out.println("subcadena2 = " + subcadena2);
+    }
+}
+
+/*
+ * NOTAS:
+ * Una subcadena es una parte extraída de una cadena original (desde un index inicial a un index final (los dos dados cómo parámetros))
+ * El caracter señalado por el último index en la cadena, no se proporciona en la subcadena.
+ */
